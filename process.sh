64ae4f4b8db238408b48255490ea488c0805fa47
matentzn <nicolas.matentzoglu@gmail.com>
2020-07-07 13:13:12 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -116,8 +116,8 @@ date
 
 echo 'Create debugging files for pipeline..'
 cd $VFB_DEBUG_DIR
-robot -vv merge --inputs "*.owl" remove --axioms "disjoint" --output $VFB_FINAL_DEBUG/vfb-dependencies-merged.owl
-robot -vv reason --reasoner ELK --input $VFB_FINAL_DEBUG/vfb-dependencies-merged.owl --output $VFB_FINAL_DEBUG/vfb-dependencies-reasoned.owl
+robot merge --inputs "*.owl" remove --axioms "disjoint" --output $VFB_FINAL_DEBUG/vfb-dependencies-merged.owl
+robot reason --reasoner ELK --input $VFB_FINAL_DEBUG/vfb-dependencies-merged.owl --output $VFB_FINAL_DEBUG/vfb-dependencies-reasoned.owl
 
 echo 'Converting all OWL files to gzipped TTL'
 cd $VFB_FINAL
