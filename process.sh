5b0378d57fe76b7db13be589b2938d99f7a1d74a
Rob Court <rcourt@ed.ac.uk>
2024-10-04 12:32:21 +0100

    adding debug to failing query

--- process.sh
+++ process.sh
@@ -184,7 +184,7 @@ date
 
 if [ "$REMOVE_EMBARGOED_DATA" = true ]; then
   echo '** Deleting embargoed data.. **'
-  robot query -f csv -i ${KB_FILE} --query ${SPARQL_DIR}/embargoed_datasets_${STAGING}.sparql ${VFB_FINAL}/embargoed_datasets.txt
+  robot -vvv query -f csv -i ${KB_FILE} --query ${SPARQL_DIR}/embargoed_datasets_${STAGING}.sparql ${VFB_FINAL}/embargoed_datasets.txt
 
   echo 'First 10 embargoed datasets: '
   head -10 ${VFB_FINAL}/embargoed_datasets.txt
